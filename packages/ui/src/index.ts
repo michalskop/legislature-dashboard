d3ef3e743dd6bd524c65b691bd2161452d12c328
Michal Skop <michal.skop@kohovolit.eu>
2026-05-05 21:37:47 +0200

    fix(ui): export SK_NRSR_PARTY_META and SK_NRSR_PARTY_COLORS from package index

--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -1,7 +1,7 @@
 export { Button } from "./components/Button";
 export { SortableMpTable } from "./components/SortableMpTable";
 export type { SortableMpTableProps } from "./components/SortableMpTable";
-export { PartyBadge, CZ_PSP_PARTY_META, CZ_PSP_PARTY_COLORS } from "./components/PartyBadge";
+export { PartyBadge, CZ_PSP_PARTY_META, CZ_PSP_PARTY_COLORS, SK_NRSR_PARTY_META, SK_NRSR_PARTY_COLORS } from "./components/PartyBadge";
 export type { PartyBadgeProps, PartyBadgeSize } from "./components/PartyBadge";
 export { PartyFace } from "./components/PartyFace";
 export type { PartyFaceProps } from "./components/PartyFace";
